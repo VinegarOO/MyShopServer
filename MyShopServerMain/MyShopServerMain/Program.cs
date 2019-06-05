b4ad72b9403307bea6e080f728159e69e7a8bf47
Максим Кучарбаев <maksim.kucharbaev@gmail.com>
2019-06-05 21:00:32 +0700

    Create menu

--- MyShopServerMain/MyShopServerMain/Program.cs
+++ MyShopServerMain/MyShopServerMain/Program.cs
@@ -10,8 +10,44 @@ namespace MyShopServerMain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow");
-            Console.ReadKey(true);
+            List<String> help = new List<String> { "start - starting server", "add - add new lot",
+                "edit - edit lot", "exit - close console", "stop - stop the server",
+                "inspect - show info about lot" };
+            while (true)
+            {
+                Console.WriteLine("Waiting for comand");
+                String[] comand = Console.ReadLine().Split();
+                switch (comand[0])
+                {
+                    case "start":
+                        Console.WriteLine("Starting server");
+                        break;
+                    case "exit":
+                        Console.WriteLine("Closing console");
+                        return;
+                    case "add":
+                        Console.WriteLine("Adding new lot to the shop");
+                        break;
+                    case "edit":
+                        Console.WriteLine("Changing a lot into the shop");
+                        break;
+                    case "inspect":
+                        Console.WriteLine("It is info");
+                        break;
+                    case "stop":
+                        Console.WriteLine("Stoping server");
+                        break;
+                    case "help":
+                        foreach (var h in help)
+                        {
+                            Console.WriteLine(h);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Not a comand");
+                        break;
+                }
+            }
         }
     }
 }
