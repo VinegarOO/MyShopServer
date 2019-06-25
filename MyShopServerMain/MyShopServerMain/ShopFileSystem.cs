2efae14a093d6dc711291030f95a10d856d7c61d
Максим Кучарбаев <maksim.kucharbaev@gmail.com>
2019-06-25 23:29:42 +0700

    Create FileSystem for serialize and deserialise shopItem. Create ShopItem class, almost creat shop class that provide any shopItem classes

---
+++ MyShopServerMain/MyShopServerMain/ShopFileSystem.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.VisualBasic.FileIO;
+
+namespace ShopFileSystems
+{
+    static class ShopFileSystem
+    {
+        static public void SaveToFile(ShopFileSystemData fsInfo)
+        {
+            String f_name = String.Concat(fsInfo.name, fsInfo.typeOfData);
+            if (File.Exists(f_name))//cheking file for exists
+            {
+                Console.WriteLine("File is exists.", Environment.NewLine, "Do you want to rewrite it?", 
+                    Environment.NewLine, "(yes/no)");
+                for (; ; )// waiting for answer
+                {
+                    String temp = Console.ReadLine();
+                    if (temp == "n" || temp == "no")
+                    {
+                        throw new FileLoadException("File already exists", f_name);
+                    }
+                    else if (temp == "y" || temp == "yes")
+                    {
+                        File.Delete(f_name);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Eror, please try again.");
+                    }
+                }
+                
+            }
+            using (FileStream fs = new FileStream(f_name, FileMode.Create))
+            {
+                Int32 numbers = fsInfo.data.Length;
+                fs.Write(BitConverter.GetBytes(numbers), 0, 4);
+                Int64 data_position = (numbers + 1) * 4;
+                for (Int32 i = 0; i < numbers; i++)
+                {
+                    fs.Write(BitConverter.GetBytes(fsInfo.data[i].Length), 0, 4);
+                    fs.Position = data_position;
+                    fs.Write(fsInfo.data[i], 0, fsInfo.data[i].Length);
+                    data_position = fs.Position;
+                    fs.Position = (i + 2) * 4;
+                }
+            }
+        }
+
+        static public ShopFileSystemData LoadFromFile(String name , String typeOfData)
+        {
+            Byte[][] data;
+            String f_name = String.Concat(name, typeOfData);
+            if (!File.Exists(f_name))
+            {
+                throw new FileNotFoundException("File is not exists", f_name);
+            }
+            Byte[] temp = new Byte[4];
+            using (FileStream fs = new FileStream(f_name, FileMode.Open))
+            {
+                fs.Read(temp, 0, 4);
+                Int32 numbers = BitConverter.ToInt32(temp, 0);
+                data = new Byte[numbers][];
+                Int64 data_position = (numbers + 1) * 4;
+                for (Int32 i = 0; i < numbers; i++)
+                {
+                    fs.Read(temp, 0, 4);
+                    fs.Position = data_position;
+                    Int32 t_leght = BitConverter.ToInt32(temp, 0);
+                    data[i] = new Byte[t_leght];
+                    fs.Read(data[i], 0, t_leght);
+                    data_position = fs.Position;
+                    fs.Position = (i + 2) * 4;
+                }
+            }
+            ShopFileSystemData fsData = new ShopFileSystemData(name, data);
+            return fsData;
+        }
+
+        static public List<String> GetAllFiles(String typeOfData)
+        {
+            String[] t_files;
+            List<String> files = new List<String>();
+            String directory = FileSystem.CurrentDirectory;
+            t_files = FileSystem.GetFiles(directory).ToArray<String>();
+            for (Int32 i = 0; i < t_files.Length; i++)
+            {
+                if (t_files[i].EndsWith(typeOfData))
+                {
+                    Int32 start = t_files[i].LastIndexOf('\\') + 1;
+                    Int32 end = t_files[i].LastIndexOf('.');
+                    String temp = t_files[i].Substring(start, end - start);
+                    files.Add(temp);
+                }
+            }
+            return files;
+        }
+    }
+
+    class ShopFileSystemData
+    {
+        public readonly String name;
+        public readonly String typeOfData = ".safer";
+        public readonly Byte[][] data;
+        private Encoding encoding = new UTF8Encoding();
+
+        public ShopFileSystemData(String t_name, Int32 price, String about, Byte[] image)
+        {
+            name = t_name;
+            data = new Byte[3][];
+            data[0] = BitConverter.GetBytes(price);
+            data[1] = encoding.GetBytes(about);
+            data[2] = image;
+        }
+
+        public ShopFileSystemData(String t_name, Byte[][] t_data)
+        {
+            if (t_data.Length != 3) throw new ArgumentException("Byte array must have only 3 members");
+            name = t_name;
+            data = t_data;
+        }
+    }
+}
+ 
---
+++ MyShopServerMain/MyShopServerMain/ShopForServer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ShopFileSystems;
+
+namespace Shop
+{
+    class Shop : IShop
+    {
+        private List<ShopLot> shopLots;
+
+        public Shop()
+        {
+            List<String> shopFiles;
+            shopFiles = ShopFileSystem.GetAllFiles(".safer");
+            shopLots = new List<ShopLot>(shopFiles.Count);
+            for (Int32 i = 0; i < shopLots.Count; i++)
+            {
+                shopLots[i] = new ShopLot(shopFiles[i]);
+                shopLots[i].LoadShopLot();
+            }
+        }
+
+        public void AddShopLot(IShopLot shopLot)
+        {
+            if (shopLot is ShopLot)
+            {
+                shopLots.Add(shopLot as ShopLot);
+            }
+            else throw new ArgumentException("IShopLot must be ShopLot", shopLot.ToString());
+        }
+
+        public void RemoveShopLot(IShopLot shopLot)
+        {
+
+        }
+    }
+
+    class ShopLot : IShopLot
+    {
+        public String Name { get; private set; }
+        public Double Price { get; private set; }
+        private String about;
+        private Byte[] picture;
+
+        public ShopLot(String name)
+        {
+            Name = name;
+        }
+
+        public void FillShopLot(Double price, String picture_path, String t_about)
+        {
+            if (price > 0) Price = price;
+            else throw new ArgumentException("price must be greater than 0", new Exception(price.ToString()));
+            FileInfo fl = new FileInfo(picture_path);
+            picture = new Byte[fl.Length];
+            using (FileStream pic = new FileStream(picture_path, FileMode.Open))
+            {
+                pic.Read(picture, 0, (Int32)fl.Length);
+            };
+            fl.Delete();
+            this.about = t_about;
+        }
+
+        public void LoadShopLot()
+        {
+            ShopFileSystemData data;
+            data = ShopFileSystem.LoadFromFile(Name, ".safer");
+            if (data.data.Length != 3) throw new InvalidDataException("must be 3 filds");
+
+        }
+
+        public void SaveShopLot()
+        {
+            
+        }
+    }
+
+    interface IShopLot
+    {
+        void LoadShopLot();
+
+        void SaveShopLot();
+    }
+
+    interface IAccount
+    {
+        void ChangePassword(String password);
+
+        void LoadAccout();
+
+        void SaveAccout();
+    }
+
+    interface IShop
+    {
+        void AddShopLot(IShopLot shopLot);
+
+        void RemoveShopLot(IShopLot shopLot);
+
+        List<IShopLot> GetShopLots();
+
+
+    }
+}
